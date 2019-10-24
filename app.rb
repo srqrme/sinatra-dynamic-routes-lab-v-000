ae385bea40305f09fe946bd9b57f236bd7ec8f3a
Sharmaine Requerme <srqrme@gmail.com>
2019-10-24 20:06:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name = params[:name].reverse
+    params[:name].reverse
   end
 
   get '/multiply/:num1/:num2' do
