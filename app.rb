75a9ff5eeb6ee07372898eabf738ed8ef9598e7d
Sharmaine Requerme <srqrme@gmail.com>
2019-10-24 20:06:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,7 @@ class App < Sinatra::Base
   end
 
   get '/multiply/:num1/:num2' do
-    multiplied = params[:num1].to_i * params[:num2].to_i
-    "#{multiplied}"
+    @multiplied = params[:num1].to_i * params[:num2].to_i
   end
 
   get '/square/:number' do
